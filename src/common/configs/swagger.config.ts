16b580a6e221897c9de6bcc61dc51f0151493a7c
Lucas Menezes <lucasmenezes4741@gmail.com>
2026-08-17 22:16:17 -0300

    feat: configura Swagger em /api/docs

---
+++ src/common/configs/swagger.config.ts
@@ -0,0 +1,12 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Noticia API')
+    .setDescription('API RESTful para gerenciamento de notícias')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+}
